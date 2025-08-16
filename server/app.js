da067b6e4a419577d0266bc636df23e8b7ffa888
matlopweb <lopezschwerterspa@gmail.com>
2025-08-16 14:15:38 -0400

    fix: Corrige la configuración de despliegue y las rutas de archivos estáticos para Render

--- server/app.js
+++ server/app.js
@@ -59,12 +59,14 @@ app.use('/api/evaluations', require('./routes/evaluations'));
 
 // In production, serve static files from the React app
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    const clientDistPath = path.join(__dirname, '../client/dist');
+
+    app.use(express.static(clientDistPath));
 
     // The "catchall" handler: for any request that doesn't
     // match one above, send back React's index.html file.
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+        res.sendFile(path.join(clientDistPath, 'index.html'));
     });
 }
 
